3bda27e52fd8be5a36e883701d9b9a5e3b1656b0
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-05-21 15:46:14 +0800

    Add simple html stuff on App and header as base

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    <>
+        <h1>TextTile</h1>
+
+        <img src="" alt="default profile" />
+        <h1>Hello, user!</h1>
+        <button>Log in</button>
+        <button>Log out</button>
+    </>
+}
+
+export default Header
