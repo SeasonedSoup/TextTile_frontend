622f3130844b022238997bc3f3935970f1230159
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-05-21 15:51:22 +0800

    Create dashboard layout html only

---
+++ src/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+
+function Dashboard() {
+    <>
+        <div className="chatChoice"></div>
+        <div className="chatLayout"></div>
+        <div className="users">
+            <button>Chat with this user each</button>
+        </div>
+        {/* This is where you message */}
+        <input className="messageBar" />
+    </>
+}
+
+export default Dashboard;
